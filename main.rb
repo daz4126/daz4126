5e0562475e56cd43ac3f025b1952dad7a4846f48
DAZ <daz4126@gmail.com>
2012-02-15 18:01:06 +0000

    changed senders field in email form

--- main.rb
+++ main.rb
@@ -64,7 +64,7 @@ end
 post '/' do
     require 'pony'
     Pony.mail(
-      from: "DAZ4126.com",
+      from: "DAZ4126",
       to: 'daz4126@gmail.com',
       subject: "A message from the DAZ4126 website",
       body: params[:message],
